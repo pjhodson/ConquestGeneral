9acdcac5d81b5cb901e53a0d15e6383f52c4943d
pjhodson <peter@thehodsons.org>
2013-11-14 14:43:54 -0800

    Main Menu!

---
+++ Assets/Scripts/mainMenu.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class mainMenu : MonoBehaviour {
+
+	public GUITexture gameLogo;
+	public GUITexture fadeBar;
+	
+	public GUIContent[] menuContent;
+	
+	private GUIStyle centeredText;
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	
+	void OnGUI()
+	{
+		centeredText = GUI.skin.GetStyle("Label");
+		centeredText.alignment = TextAnchor.UpperCenter;
+		centeredText.fontSize = 18;
+				
+		Rect inset = new Rect(0, Screen.height - gameLogo.pixelInset.height, gameLogo.pixelInset.width,gameLogo.pixelInset.height);
+		gameLogo.pixelInset = inset;
+		
+		inset = new Rect(Screen.width/2 - 300, 0, fadeBar.pixelInset.width,fadeBar.pixelInset.height);
+		fadeBar.pixelInset = inset;
+		
+		for(int i = 0; i < menuContent.Length; i++)
+		{
+			if(GUI.Button (new Rect(Screen.width/2 - 75, Screen.height/2 - 15 + 35*i, 150,30),menuContent[i]))
+			{
+				handleMenu(i);
+			}
+			GUI.Label (new Rect(Screen.width/2 - 200, Screen.height - 30, 400, 36), GUI.tooltip, centeredText);
+		}
+	}
+	void handleMenu(int i)
+	{
+		switch(i)
+		{
+		case 0: //play
+			Application.LoadLevel("ConquestGeneral");
+			break;
+		case 1: //how-to
+			Debug.Log ("COMING SOON");
+			break;
+		case 2:
+			Application.Quit();
+			break;
+		default:
+			break;
+		}
+	}
+}
